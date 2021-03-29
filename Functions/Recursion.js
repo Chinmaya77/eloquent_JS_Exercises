16a8a92554b4659d8e31292efc0deda5d825621c
Chinmaya77 <chinmaya.mn@robosoftin.com>
2021-03-29 12:53:17 +0530

    adding functions exercises

---
+++ Functions/Recursion.js
@@ -0,0 +1,11 @@
+"use strict";
+let isEven = function (n) {
+  if (n === 0) {
+    console.log("true");
+  } else if (n === 1) {
+    console.log("false");
+  } else {
+    isEven(n - 2);
+  }
+};
+isEven(12);
